ad6cddec706588dbec14dc810c8a29c366268d36
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-04-14 15:51:30 +0200

    Optimized Theme Module
    - Split the previous code from MainLayout into a separate module file for the theme settings, so it could be used by multiple components as a layout base.
    
    - Still issues with dark theme not being applied on the loading screen when the app is being initialized.

--- Portfolio/Portfolio.Website/wwwroot/js/document-settings.js
+++ Portfolio/Portfolio.Website/wwwroot/js/document-settings.js
@@ -5,3 +5,18 @@
 
     document.title = title;
 }
+
+export function setDocumentLangCode() {
+    const culture = localStorage.getItem("culture");
+
+    if (culture == null) {
+        console.warn("Culture settings was null, setting default language to 'en'.");
+
+        document.documentElement.setAttribute("lang", "en");
+        return;
+    }
+
+    const languageCode = culture.split("-")[0];
+
+    document.documentElement.setAttribute("lang", languageCode);
+}
