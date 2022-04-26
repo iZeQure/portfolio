6d9fa96df95f126b8cbdff8c401766bc72755966
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-04-26 21:39:14 +0200

    Fixed issue regarding theme update
    - Fixed issue where the theme wouldn't change theme, if it was changed on the system.
    - Improved the dot net reference handler, to be invoked properly from JavaScript.

--- Portfolio/Portfolio.Website/wwwroot/js/main.js
+++ Portfolio/Portfolio.Website/wwwroot/js/main.js
@@ -1,22 +1,38 @@
-﻿let helper = null;
+﻿window.setDotNetReferenceForThemeSelector = (dotNetReference) => {
+    const ref = dotNetReference;
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
+        if (ref == null) {
+            return;
+        }
 
-window.setMediaReference = (dotNetHelper) => {
-    helper = dotNetHelper;
+        const isDarkTheme = event.matches ? true : false;
+
+        try {
+            ref.invokeMethodAsync("SystemThemeChanged", isDarkTheme)
+                .then(() => {
+                    console.info(`System theme was changed, website was updated!`);
+                });
+        } catch (e) {
+            console.error(`Failed to update website theme, on system changes.`);
+        }
+    });
 }
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
-    if (helper == null) {
-        return;
-    }
+window.setDotNetReferenceForLanguageSelector = (dotNetReference) => {
+    const ref = dotNetReference;
+    window.matchMedia("(min-width: 768px)").addEventListener('change', event => {
+        if (ref == null) {
+            console.warn("Dotnet help reference is null.");
+            return;
+        }
 
-    const isDarkTheme = event.matches ? true : false;
-
-    try {
-        helper.invokeMethodAsync("SystemThemeChanged", isDarkTheme)
-            .then(data => {
-                console.info(`System theme was changed, website was updated!`);
-            });
-    } catch (e) {
-        console.error(`Failed to update website theme, on system changes.`);
-    }
-});
+        try {
+            ref.invokeMethodAsync("InitializeMobileDeviceMode", event.matches ? false : true)
+                .then(() => {
+                    console.info("Updated device mode.");
+                });
+        } catch (e) {
+            console.error("Unsupported error occurred: {0}", e);
+        }
+    });
+}
